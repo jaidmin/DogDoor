73460c23a22e5f98f9b57d3bca32447007554eac
jaidmin <jaidmin@localhost.localdomain>
2016-12-29 11:43:29 +0100

    Initial commit

---
+++ DogDoor/DogDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DogDoor
+{
+    public class DogDoor
+    {
+        public bool Open { get; set; }
+
+        public DogDoor()
+        {
+            Open = false;
+        }
+
+        public void OpenDoor()
+        {
+            Open = true;
+        }
+
+        public void CloseDoor()
+        {
+            Open = false;
+        }
+
+        public bool IsOpen()
+        {
+            return Open;
+        }
+    }
+}
---
+++ DogDoor/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DogDoor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DogDoor")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7F00146D-6C9E-4504-8E81-6B91F1134017")]
